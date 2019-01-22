b9ac65845d49dea77e5929f0e178bbf2e19163c9
Danny Baumann <dannybaumann@web.de>
2019-01-22 14:15:40 +0100

    sdm845-common: Replace lineagehw implementation by HIDL services
    Move DisplayModeControl and SunlightEnhancement into a livedisplay
    service
    
    Change-Id: I5f46671633a13ddc6733a47f4ea5a6515d6d6c98
    Signed-off-by: Aditya Jadaun <aadijaadaun@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,9 +86,8 @@ TARGET_USES_HWC2 := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# Lineage Hardware
-JAVA_SOURCE_OVERLAYS := \
-    org.lineageos.hardware|$(COMMON_PATH)/lineagehw|**/*.java
+# HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 
 # NFC
 TARGET_USES_NQ_NFC := true
