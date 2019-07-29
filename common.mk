a4da2f06fec5e0e4a389aae6d5c1c0c08191b3c9
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-07-30 02:32:23 +0530

    sdm845-common: Introduce DeviceSettings
    combine DisplayMode and KeyHandler to one category containing all device specific options
    
    [ anirudhgupta109 | AOSiP ]:
    - Modify and adapt to sdm845-common
    - Revert commit f8408dd933c255fd099644ddd9e9313ff9725dc1 as it broke Silent mode
    
    Change-Id: I3f1ae8adb9e86db1a505acb43c837bbeea6f984b
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Aditya Jadaun <aadijaadaun@gmail.com>
    Signed-off-by: Rahul Singh <rahul99jaykar00@gmail.com>

--- common.mk
+++ common.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     AntHalService
 
+# AOSiP Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default
@@ -72,8 +76,7 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    libvulkan \
-    DisplayMode
+    libvulkan
 
 # Doze
 PRODUCT_PACKAGES += \
@@ -137,7 +140,6 @@ PRODUCT_PACKAGES += \
 
 # tri-state-key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon
 
 # Update engine
